8d90aa2e761108003e8830002b36c42e14d1811c
Xixiqo <yazhar363@gmail.com>
2026-02-26 08:41:15 +0700

    add: make_product_request

---
+++ app/Http/Requests/ProductRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProductRequest extends FormRequest
+{
+    /**
+     * Tentukan siapa yang boleh pakai request ini.
+     */
+    public function authorize(): bool
+    {
+        // false artinya semua request **tidak otomatis diizinkan**
+        return false;
+    }
+
+    /**
+     * Rules untuk validasi input.
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'price' => 'required|numeric',
+            'stock' => 'required|integer|min:0',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+        ];
+    }
+}
